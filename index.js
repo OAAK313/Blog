19ea385e4783c9b281fbc9de223c9148d8c07f64
OAAK313 <akasha.zhto@gmail.com>
2024-06-19 03:28:55 +0500

    Update index.js

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ function main() {
 
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(express.static("public"));
+  app.set('view engine', 'ejs');
 
   app.listen(port, () => {
     console.log(`Listening on port ${port}`);
