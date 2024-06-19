c1af039758c1ec8dcd0743290306f26ca1adae42
OAAK313 <akasha.zhto@gmail.com>
2024-06-19 17:16:24 +0500

    Update index.js

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ function main() {
     //   res.render("view.ejs", { post });
     // }
     // res.render("view.ejs", { post });
-      res.send(req.body.postTitle, req.body.postContent);
+      res.send(post.ID);
   });
 
   app.post("/create", (req, res) => {
